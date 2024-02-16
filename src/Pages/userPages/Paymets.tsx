1a3802d663f26a849c05cdbf171e56319b5f2bf1
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-02-16 23:43:41 +0530

    ui changes

--- src/Pages/userPages/Paymets.tsx
+++ src/Pages/userPages/Paymets.tsx
@@ -6,6 +6,7 @@ import User from "../../interfaces/user";
 import axios from "../../Services/axios";
 import toast from "react-hot-toast";
 import { PaymentPopulated } from "../../interfaces/payment";
+import image from "../../../public/images/Make it rain-bro.png";
 
 const Paymets = () => {
     const userData: User | null = useAppSelector((state) => state.user.user);
@@ -46,77 +47,90 @@ const Paymets = () => {
                         Pending Payments
                     </p>
                 </div>
-                <div className="my-5 grid grid-cols-3 gap-3">
-                    {payments?.map((payment) => (
-                        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-                                {payment?.purpose}
-                            </h5>
-                            <div className="flex items-baseline text-gray-900 dark:text-white">
-                                <span className="text-3xl font-semibold">
-                                    ₹
-                                </span>
-                                <span className="text-5xl font-extrabold tracking-tight">
-                                    {payment?.amount}
-                                </span>
-                                {/* <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+                {payments && payments.length > 0 ? (
+                    <div className="my-5 grid grid-cols-3 gap-3">
+                        {payments?.map((payment) => (
+                            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                                <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+                                    {payment?.purpose}
+                                </h5>
+                                <div className="flex items-baseline text-gray-900 dark:text-white">
+                                    <span className="text-3xl font-semibold">
+                                        ₹
+                                    </span>
+                                    <span className="text-5xl font-extrabold tracking-tight">
+                                        {payment?.amount}
+                                    </span>
+                                    {/* <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                                 /month
                             </span> */}
+                                </div>
+                                <ul role="list" className="space-y-5 my-7">
+                                    <li className="flex items-center">
+                                        <svg
+                                            className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                        </svg>
+                                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                                            {`Project: ${payment?.project.projectName}`}
+                                        </span>
+                                    </li>
+                                    <li className="flex">
+                                        <svg
+                                            className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                        </svg>
+                                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                                            Service charge
+                                        </span>
+                                    </li>
+                                    <li className="flex">
+                                        <svg
+                                            className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                        </svg>
+                                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+                                            Integration help
+                                        </span>
+                                    </li>
+                                </ul>
+                                <button
+                                    type="button"
+                                    onClick={() => handlePay(payment._id)}
+                                    className="text-white bg-gradient-to-r from-emerald-400 via-emerald-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+                                >
+                                    Pay Now
+                                </button>
                             </div>
-                            <ul role="list" className="space-y-5 my-7">
-                                <li className="flex items-center">
-                                    <svg
-                                        className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                    </svg>
-                                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                                        {`Project: ${payment?.project.projectName}`}
-                                    </span>
-                                </li>
-                                <li className="flex">
-                                    <svg
-                                        className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                    </svg>
-                                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                                        Service charge
-                                    </span>
-                                </li>
-                                <li className="flex">
-                                    <svg
-                                        className="flex-shrink-0 w-4 h-4 text-emerald-500 dark:text-emerald-500"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                                    </svg>
-                                    <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                                        Integration help
-                                    </span>
-                                </li>
-                            </ul>
-                            <button
-                                type="button"
-                                onClick={() => handlePay(payment._id)}
-                                className="text-white bg-gradient-to-r from-emerald-400 via-emerald-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-                            >
-                                Pay Now
-                            </button>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className=" flex flex-col justify-center items-center m-4">
+                        <img
+                            src={image}
+                            alt="Project Image"
+                            className="max-w-full w-[30%] h-auto rounded-md"
+                        />
+                        <p className="mt-4 text-lg text-gray-400">
+                            No pending payments on your account.
+                        </p>
+                    </div>
+                )}
             </div>
         </UserSideBar>
     );
--- src/Pages/userPages/UserProjects.tsx
+++ src/Pages/userPages/UserProjects.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from "../../Services/redux/hooks";
 import projectApi from "../../Services/apis/projectApi";
 import Spinner from "../../components/common/Spinner";
 import { Link } from "react-router-dom";
+import image from "../../../public/images/Shrug-bro.png";
 
 const UserProjects = () => {
     const [projects, setProjects] = useState<Project[]>();
@@ -37,78 +38,91 @@ const UserProjects = () => {
                     <Spinner />
                 ) : (
                     <div className="p-5">
-                        <div className="flex flex-row align-middle items-center">
+                        <div className="flex flex-row w-full h-full align-middle items-center">
                             <h2 className="text-2xl font-semibold text-stone-200 pl-5 pt-5">
                                 Projects
                             </h2>
                         </div>
                         <div>
-                            <div className="flex flex-wrap">
-                                {/* Box for Project 1 */}
-                                {projects?.map((project) => {
-                                    return (
-                                        <div
-                                            key={project._id}
-                                            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-4"
-                                        >
-                                            <Link
-                                                to={`/projects/${project._id}`}
+                            {projects && projects?.length > 0 ? (
+                                <div className="flex flex-wrap">
+                                    {projects?.map((project) => {
+                                        return (
+                                            <div
+                                                key={project._id}
+                                                className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-4"
                                             >
-                                                <div className="relative z-0 flex bg-clip-border rounded-xl bg-gray-900 text-gray-700 shadow-md w-full max-w-[30rem] min-w-[20rem] border-b border-red-500 border-6">
-                                                    <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-                                                        <img
-                                                            src="https://www.shutterstock.com/image-illustration/architecture-3d-rendering-illustration-minimal-260nw-2274549397.jpg"
-                                                            alt="card-image"
-                                                            className="object-cover w-full h-full"
-                                                        />
-                                                    </div>
-                                                    <div className="p-6">
-                                                        <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-gray-400">
-                                                            {
-                                                                project.projectName
-                                                            }
-                                                        </h4>
-                                                        <p className="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-400">
-                                                            Like so many
-                                                            organizations these
-                                                            days, Autodesk is a
-                                                            company in
-                                                            transition.
-                                                        </p>
-                                                        <a
-                                                            href="#"
-                                                            className="inline-block"
-                                                        >
-                                                            <button
-                                                                className="flex items-center gap-1 px-6 py-1 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
-                                                                type="button"
+                                                <Link
+                                                    to={`/projects/${project._id}`}
+                                                >
+                                                    <div className="relative z-0 flex bg-clip-border rounded-xl bg-gray-900 text-gray-700 shadow-md w-full max-w-[30rem] min-w-[20rem] border-b border-red-500 border-6">
+                                                        <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
+                                                            <img
+                                                                src="https://www.shutterstock.com/image-illustration/architecture-3d-rendering-illustration-minimal-260nw-2274549397.jpg"
+                                                                alt="card-image"
+                                                                className="object-cover w-full h-full"
+                                                            />
+                                                        </div>
+                                                        <div className="p-6">
+                                                            <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-gray-400">
+                                                                {
+                                                                    project.projectName
+                                                                }
+                                                            </h4>
+                                                            <p className="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-400">
+                                                                Like so many
+                                                                organizations
+                                                                these days,
+                                                                Autodesk is a
+                                                                company in
+                                                                transition.
+                                                            </p>
+                                                            <a
+                                                                href="#"
+                                                                className="inline-block"
                                                             >
-                                                                Learn More
-                                                                <svg
-                                                                    xmlns="http://www.w3.org/2000/svg"
-                                                                    fill="none"
-                                                                    viewBox="0 0 24 24"
-                                                                    stroke="currentColor"
-                                                                    strokeWidth={
-                                                                        2
-                                                                    }
-                                                                    className="w-4 h-4"
+                                                                <button
+                                                                    className="flex items-center gap-1 px-6 py-1 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
+                                                                    type="button"
                                                                 >
-                                                                    <path
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                                                                    />
-                                                                </svg>
-                                                            </button>
-                                                        </a>
+                                                                    Learn More
+                                                                    <svg
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        fill="none"
+                                                                        viewBox="0 0 24 24"
+                                                                        stroke="currentColor"
+                                                                        strokeWidth={
+                                                                            2
+                                                                        }
+                                                                        className="w-4 h-4"
+                                                                    >
+                                                                        <path
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                                                                        />
+                                                                    </svg>
+                                                                </button>
+                                                            </a>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    );
-                                })}
-                            </div>
+                                                </Link>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            ) : (
+                                <div className=" flex flex-col justify-center items-center m-4">
+                                    <img
+                                        src={image}
+                                        alt="Project Image"
+                                        className="max-w-full w-[30%] h-auto rounded-md"
+                                    />
+                                    <p className="mt-4 text-lg text-gray-400">
+                                        Currently, no projects are available.
+                                    </p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 )}
