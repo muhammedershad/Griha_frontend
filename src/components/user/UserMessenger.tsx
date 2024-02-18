85cbfbac93e0bf3a07e4a0866de9e0a9285d7db2
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-02-19 01:38:26 +0530

    messenger update

--- src/components/user/UserMessenger.tsx
+++ src/components/user/UserMessenger.tsx
@@ -62,7 +62,7 @@ function UserMessenger() {
                         }
                     })();
                 } else {
-                    logout();
+                    // logout();
                 }
             };
             updateSlice();
